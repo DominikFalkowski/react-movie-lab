dd7ea193ddcb4a5037c7e4dc43218b941287c23c
Dominik Falkowski <20102969@mail.wit.ie>
2024-11-03 18:36:11 +0000

    fix backward and forward arrows

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
+
 
 
 const App = () => {
