c13172cda2d010deeaa95ad0036fac0aa1078faf
Dominik Falkowski <20102969@mail.wit.ie>
2024-11-03 18:04:45 +0000

    Add to favorite logic completed

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -22,6 +22,12 @@ const HomePage = (props) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
   };
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
 
   useEffect(() => {
     fetch(
@@ -50,8 +56,8 @@ const HomePage = (props) => {
             genreFilter={genreFilter}
         />
         </Grid>
-        <MovieList movies={displayedMovies} />
-              </Grid>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+        </Grid>
     </Grid>
   );
 };
